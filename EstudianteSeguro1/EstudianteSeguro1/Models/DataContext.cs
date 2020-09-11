fef00df7eaa9fd063a75a671ed276d6eb45c594d
GEAN MARCO VALVERDE ARANA <a2019114321@estudiantes.upsa.edu.bo>
2020-09-11 18:03:27 -0400

    Con controlador students

---
+++ EstudianteSeguro1/EstudianteSeguro1/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿
+namespace EstudianteSeguro1.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<EstudianteSeguro1.Models.Student> Students { get; set; }
+    }
+}
